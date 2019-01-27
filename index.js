814a829721889c804540c51e9d63803026085ab5
Veronica Bustabad <vbustabad@gmail.com>
2019-01-27 01:26:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,33 +1,4 @@
-function inOrder(currentNode){
-  if(currentNode.left){
-    inOrder(currentNode.left)
-  }
-  console.log(currentNode.data)
-  if(currentNode.right){
-    inOrder(currentNode.right)
-  }
-}
 
-function findOrAdd(rootNode, newNode){
-  let currentNode = rootNode;
-  if(newNode.data < rootNode.data){
-      currentNode = rootNode.left;
-      if(currentNode){
-        findOrAdd(currentNode, newNode);
-        return true;
-      } else {
-        rootNode.left = newNode;
-      }
-  } else if(newNode.data > rootNode.data) {
-    currentNode = rootNode.right;
-    if(currentNode){
-      findOrAdd(currentNode, newNode);
-      return true;
-    } else {
-      rootNode.right = newNode;
-    }
-  }
-}
 
 function max(node) {
   if (node.right !== null) {
